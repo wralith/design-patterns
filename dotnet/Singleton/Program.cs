c21545f77ba5c0122a54391ed89ee4e7c5461441
Recep Gunes <recepgunes2@outlook.com>
2023-06-25 20:30:09 +0300

    add singleton pattern in c#, non-thread-safe

---
+++ dotnet/Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton;
+
+var firstLogger = Logger.GetInstance();
+var secondLogger = Logger.GetInstance();
+
+if (firstLogger == secondLogger)
+{
+    Console.WriteLine("they are same.");
+}
